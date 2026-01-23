6e4f51456f135c9828b4a5ea97a74c017cbb4685
Nikola Đapić <nikola.djapic@soprex.com>
2026-01-23 09:35:18 +0100

    DTO and config updates

--- SuperLeague/DTOs/CreateTeamDto.cs
+++ SuperLeague/DTOs/CreateTeamDto.cs
@@ -6,16 +6,16 @@ namespace SuperLeague.DTOs
     {
         [Required(ErrorMessage = "You didn't populate team name.")]
         [StringLength(20, ErrorMessage = "Team cannot exceed 20 characters.")]
-        public string TeamName { get; set; }
+        public string TeamName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "You didn't populate team date of foundation.")]
-        public DateTime DateOfFoundation { get; set; }
+        public DateTime DateOfFoundation { get; set; } 
         [Required(ErrorMessage = "You didn't enter team stadium.")]
         [StringLength(30, ErrorMessage = "Stadium cannot exceed 30 characters.")]
-        public string Stadium { get; set; }
+        public string Stadium { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "You didn't enter team city.")]
         [StringLength(20, ErrorMessage = "Team cannot exceed 20 characters.")]
-        public string City { get; set; }
+        public string City { get; set; } = string.Empty;
     }
 }
